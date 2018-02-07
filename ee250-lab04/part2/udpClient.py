4d3de03dab8a975d511f401d42c8db5bc95bcea5
ee250 <shmcdono@usc.edu>
2018-02-06 18:10:38 -0800

    fix host and server addr

--- ee250-lab04/part2/udpClient.py
+++ ee250-lab04/part2/udpClient.py
@@ -3,10 +3,10 @@ import socket
 def Main():
     # Change the host and port as needed. For ports, use a number in the 9000 
     # range. 
-    host = '127.0.0.1'
+    host = '192.168.1.103'
     port = 5001
 
-    server_addr = '127.0.0.1'
+    server_addr = '192.168.1.190'
 
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
     s.bind((host,port))
