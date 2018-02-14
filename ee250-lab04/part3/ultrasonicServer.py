cda9fa52499d4dc8409d6975d2bdbc294842f4db
ee250 <shmcdono@usc.edu>
2018-02-13 16:43:22 -0800

    ultrasonic copy and past from part 2

--- ee250-lab04/part3/ultrasonicServer.py
+++ ee250-lab04/part3/ultrasonicServer.py
@@ -4,5 +4,28 @@ import sys
 sys.path.append('../../Software/Python/')
 
 from grovepi import *
+import socket
 
 #use UDP
+def Process1():
+    # Change the host and port as needed. For ports, use a number in the 9000 
+    # range. 
+    host = '10.0.2.15'
+    port = 9000
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((host,port))
+
+    print("Process 1 Server Started")
+    while True:
+        data, addr = s.recvfrom(1024)
+        data = data.decode('utf-8')
+        print("Message From: " + str(addr))
+        print("From connected user: " + data)
+        data = data.upper()
+        print("Sending: " + data)
+        s.sendto(data.encode('utf-8'), addr)
+    c.close()
+
+if __name__ == '__main__':
+    Process1()
