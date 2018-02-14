881124720ade54ee7b44a3a08b9e512ddc74a872
ee250 <shmcdono@usc.edu>
2018-02-13 17:30:33 -0800

    fix indentation error attempt 3

--- ee250-lab04/part3/ultrasonicClient.py
+++ ee250-lab04/part3/ultrasonicClient.py
@@ -7,38 +7,36 @@ from grovepi import *
 import socket
 
 #use UDP
-def Main():
-    # Change the host and port as needed. For ports, use a number in the 9000 
-    # range. 
-    host = '192.168.1.249'
-    port = 5000
+	def Main():
+	# Change the host and port as needed. For ports, use a number in the 9000 
+	# range. 
+	host = '192.168.1.249'
+	port = 5000
 
-    server_addr = '192.168.1.189'
+	server_addr = '192.168.1.189'
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
-    s.bind((host,port))
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+	s.bind((host,port))
 
-    # UDP is connectionless, so a client does not formally connect to a server
-    dst_port = 8000
+	# UDP is connectionless, so a client does not formally connect to a server
+	dst_port = 8000
 
-    while True:
-		dist = grovepi.ultrasonicRead(ultrasonic_ranger)
+	while True:
+		try:
+			# Read distance value from Ultrasonic
+			dist = grovepi.ultrasonicRead(ultrasonic_ranger)
 
-		# try:
-		#     # Read distance value from Ultrasonic
-		#     dist = grovepi.ultrasonicRead(ultrasonic_ranger)
-
-		# except TypeError:
-		#     dist = "TypeError"
-		# except IOError:
-		#     dist = "IOError"
+		except TypeError:
+			dist = "TypeError"
+		except IOError:
+			dist = "IOError"
 
 		#tuples are immutable so we need to overwrite the last tuple
 		server = (server_addr, int(dst_port))
 
 		# for UDP, sendto() and recvfrom() are used instead
 		s.sendto(dist.encode('utf-8'), server) 
-    s.close()
+	s.close()
 
-if __name__ == '__main__':
-    Main()
+	if __name__ == '__main__':
+		Main()
