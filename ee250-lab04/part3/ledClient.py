dcdf5cc9e960727bae1ecce93532a6e21d3d3cd4
ee250 <shmcdono@usc.edu>
2018-02-13 19:22:45 -0800

    LED client and server

--- ee250-lab04/part3/ledClient.py
+++ ee250-lab04/part3/ledClient.py
@@ -3,4 +3,37 @@ import sys
 # we are successfully `from grovepi import *`
 sys.path.append('../../Software/Python/')
 
+import grovepi
 from grovepi import *
+
+
+#use tcp
+def Main():
+	host = '192.168.1.249'
+	port = 5000
+
+	s = socket.socket()
+	print("socket created")
+
+	s.connect((host,port))
+	print("socket connected")
+
+	LED = 3
+
+	pinMode(LED,"OUTPUT")
+
+	message = "LED waiting for command..."
+	while True:
+		s.send(message.encode('utf-8'))
+		data = s.recv(1024).decode('utf-8')
+		if(data is "LED_ON"):
+			digitalWrite(LED,1)
+			message = "LED ON"
+		if(data is "LED_OFF"):
+			message = "LED OFF"
+		else:
+			message = "command not recognized"
+	s.close()
+
+if __name__ == '__main__':
+	Main()
--- ee250-lab04/part3/ledServer.py
+++ ee250-lab04/part3/ledServer.py
@@ -3,6 +3,30 @@ import sys
 # we are successfully `from grovepi import *`
 sys.path.append('../../Software/Python/')
 
-from grovepi import *
+import socket 
 
 # use TCP
+def Process1():
+    # Change the host and port as needed. For ports, use a number in the 9000 
+    # range. 
+    host = '10.0.2.15'
+    port = 9000
+
+    s = socket.socket()
+    s.bind((host,port))
+
+    s.listen(1)
+    c, addr = s.accept()
+
+    print("Connected to: " + str(addr))
+
+    command = input("command->")
+    while True:
+        c.send(command.encode('utf-8'))
+        data = c.recv(1024).decode('utf-8')
+        print("LED status: " + data)
+        command = input("command->")
+    c.close()
+
+if __name__ == '__main__':
+    Process1()
